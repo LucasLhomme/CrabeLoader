09e7c348a2462eb82013c1b9af1c498962f35f98
Crabe <lucas.lhomme@epitech.eu>
2026-09-17 02:22:52 +0200

    fix(ci): target Win32, ignore generated embedded_api.hpp, and eliminate compiler warnings

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,7 @@ void initMain() {
 
 bool APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
+    (void)lpReserved;
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hModule);
         initLogger(hModule);
